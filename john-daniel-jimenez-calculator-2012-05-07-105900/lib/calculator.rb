c9de96d2a3f72b0eb49c23db4c22cbe1c94a6adc
Wes Bailey <baywes@gmail.com>
2012-05-07 11:21:44 -0700

    first stanza

--- john-daniel-jimenez-calculator-2012-05-07-105900/lib/calculator.rb
+++ john-daniel-jimenez-calculator-2012-05-07-105900/lib/calculator.rb
@@ -1,2 +1,12 @@
 class Calculator
+  def initialize expression
+    @expression = expression
+  end
+  def expr
+    @expression
+  end
+  def expr= expression
+    @expression = expression
+  end
+
 end
--- john-daniel-jimenez-calculator-2012-05-07-105900/spec/calculator_spec.rb
+++ john-daniel-jimenez-calculator-2012-05-07-105900/spec/calculator_spec.rb
@@ -3,10 +3,29 @@ require 'calculator'
 
 describe Calculator do
   describe "new" do
-    it "should instantiate" do
+
+    it "require expression" do
       expect {
         Calculator.new
+      }.should raise_exception
+    end
+
+    it 'accepts expression' do
+      expect {
+        Calculator.new '1,2'
       }.should_not raise_exception
+    end
+
+    it 'expression is accessible via expr' do
+      c = Calculator.new '1,2'
+      c.expr.should == '1,2'
+    end
+
+    it 'allows assignment' do
+      c = Calculator.new '1,2'
+      c.expr.should == '1,2'
+      c.expr = '3,4'
+      c.expr.should == '3,4'
     end
   end
 end
