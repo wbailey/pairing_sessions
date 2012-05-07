2695bda62bec9ee67f461ae1558e2965f3d90284
Wes Bailey <baywes@gmail.com>
2012-05-07 11:32:01 -0700

    second stanza

--- john-daniel-jimenez-calculator-2012-05-07-105900/lib/calculator.rb
+++ john-daniel-jimenez-calculator-2012-05-07-105900/lib/calculator.rb
@@ -2,11 +2,22 @@ class Calculator
   def initialize expression
     @expression = expression
   end
+
   def expr
     @expression
   end
+
   def expr= expression
     @expression = expression
   end
 
+  def add 
+    if @expression == ''
+      return 0
+    elsif @expression.match /^\s*(\d+)\s*$/
+      return $1.to_i
+    elsif @expression.match /^\s*(\d+),(\d+)\s*$/
+      return $1.to_i + $2.to_i
+    end
+  end
 end
--- john-daniel-jimenez-calculator-2012-05-07-105900/spec/calculator_spec.rb
+++ john-daniel-jimenez-calculator-2012-05-07-105900/spec/calculator_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'calculator'
 
 describe Calculator do
-  describe "new" do
+  describe "#new" do
 
     it "require expression" do
       expect {
@@ -28,4 +28,23 @@ describe Calculator do
       c.expr.should == '3,4'
     end
   end
+
+  describe '#add' do
+    it 'evaluates empty expression' do
+      c = Calculator.new ''
+      c.add.should == 0
+    end
+
+    it 'evaluates single character expression' do
+      c = Calculator.new '1'
+      c.add.should == 1
+    end
+
+    it 'evaluates two character expression' do
+      c = Calculator.new ''
+      c.expr = '1,2'
+      c.add.should == 3
+    end
+  end
+
 end
