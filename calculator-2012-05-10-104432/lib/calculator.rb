72c7a1cfc991d3a5c6f1c56983db35429bc598e9
Wes Bailey <baywes@gmail.com>
2012-05-10 11:26:35 -0700

    first stanza

--- calculator-2012-05-10-104432/lib/calculator.rb
+++ calculator-2012-05-10-104432/lib/calculator.rb
@@ -1,2 +1,16 @@
 class Calculator
+
+  def initialize(expressn)
+    args = expressn.split(/,/)
+    @expr = expressn
+    raise "Invalid expression" unless  (args[0] =~ /^\d+$/) && (args[1] =~ /^\d+$/)
+  end
+
+  def expr
+    @expr
+  end
+
+  def expr= (expressn)
+    @expr = expressn
+  end
 end
--- calculator-2012-05-10-104432/spec/calculator_spec.rb
+++ calculator-2012-05-10-104432/spec/calculator_spec.rb
@@ -6,7 +6,38 @@ describe Calculator do
     it "should instantiate" do
       expect {
         Calculator.new
+      }.should raise_exception
+    end
+
+    it 'accepts valid expression' do
+      expect {
+        Calculator.new '1,2'
       }.should_not raise_exception
+
+      expect {
+        Calculator.new '3,4'
+      }.should_not raise_exception
+    end
+
+    it 'rejects invalid expresssions' do
+      expect {
+        Calculator.new 'a,v'
+      }.should raise_exception
+    end
+  end
+
+  describe '#expr' do
+    it 'access expression' do
+      c = Calculator.new '1,2'
+      c.expr.should == '1,2'
+    end
+  end
+
+  describe '#expr=' do
+    it 'assigns expression' do
+      c = Calculator.new '1,2'
+      c.expr = '3,4'
+      c.expr.should == '3,4'
     end
   end
 end
