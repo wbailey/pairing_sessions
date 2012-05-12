f3856cf255ca0da996b7a8e21c97a1ea465f94e6
Wes Bailey <baywes@gmail.com>
2012-05-11 21:53:34 -0700

    Create a calculator that is initialized with a string expression

--- calculator-2012-05-11-214906/spec/calculator_spec.rb
+++ calculator-2012-05-11-214906/spec/calculator_spec.rb
@@ -2,10 +2,16 @@ require 'spec_helper'
 require 'calculator'
 
 describe Calculator do
-  describe "new" do
-    it "should instantiate" do
+  describe "#new" do
+    it 'should instantiate' do
       expect {
         Calculator.new
+      }.should_not raise_exception
+    end
+
+    it 'accepts expression' do
+      expect {
+        Calculator.new '1,2'
       }.should_not raise_exception
     end
   end
