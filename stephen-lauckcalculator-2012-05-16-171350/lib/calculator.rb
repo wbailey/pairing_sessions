577ce203692204845debec31e02af7997d15a523
Wes Bailey <baywes@gmail.com>
2012-05-16 18:36:12 -0700

    first pass at add method

--- stephen-lauckcalculator-2012-05-16-171350/lib/calculator.rb
+++ stephen-lauckcalculator-2012-05-16-171350/lib/calculator.rb
@@ -1,7 +1,7 @@
 class Calculator
 
-  def initialize(exp)
-    @expr = exp
+  def initialize(exp='')
+    @expr = exp if self.valid?(exp)
   end
 
   def expr
@@ -9,6 +9,27 @@ class Calculator
   end
 
   def expr=(exp)
-    @expr = exp
+    @expr = exp if self.valid?(exp)
   end
+
+  def add
+    sum = 0
+    if @expr == ''
+      return sum
+    else
+      nums = @expr.split(',')
+      nums.each do |n|
+        sum = sum + n.to_i
+      end
+    end
+    sum
+  end
+
+  def valid?(exp)
+    nums = exp.split(',')
+    nums.each do |n|
+      raise Exception, 'only digits allowed' unless n.match(/[0-9,]/)
+    end
+  end
+
 end
--- stephen-lauckcalculator-2012-05-16-171350/spec/calculator_spec.rb
+++ stephen-lauckcalculator-2012-05-16-171350/spec/calculator_spec.rb
@@ -6,13 +6,19 @@ describe Calculator do
     it "should instantiate" do
       expect {
         Calculator.new
-      }.should raise_exception
+      }.should_not raise_exception
     end
 
     it 'accepts expression' do
       expect {
         Calculator.new '1,2'
       }.should_not raise_exception
+    end
+
+    it 'only accepts valid expressions' do
+      expect {
+        c = Calculator.new 'a,b'
+      }.should raise_exception
     end
   end
 
@@ -29,6 +35,33 @@ describe Calculator do
       c.expr.should == '1,2'
       c.expr = '3,4'
       c.expr.should == '3,4'
+    end
+
+    it 'only accepts valid expressions' do
+      c = Calculator.new
+
+      expect {
+        c.expr = 'a,b'
+      }.should raise_exception
+    end
+  end
+
+  describe '#add' do
+    it 'computes the emptry string' do
+      c = Calculator.new
+      c.add.should == 0
+    end
+
+    it 'computes a single digit expression' do
+      c = Calculator.new '1'
+      c.add.should == 1
+    end
+
+    it 'computes multi-digit expression' do
+      c = Calculator.new '1,2'
+      c.add.should == 3
+      c.expr = '7,8'
+      c.add.should == 15
     end
   end
 end
