aafefb223e999dd9b5a57472fb0324dbda1dfb13
Wes Bailey <baywes@gmail.com>
2012-05-17 13:15:49 -0700

    Create a calculator that is initialized with a string expression

--- benjamin-marini-calculator-2012-05-17-130338/spec/calculator_spec.rb
+++ benjamin-marini-calculator-2012-05-17-130338/spec/calculator_spec.rb
@@ -8,5 +8,26 @@ describe Calculator do
         Calculator.new
       }.should_not raise_exception
     end
+
+    it 'accepts expression' do
+      expect {
+        Calculator.new '1,2'
+      }.should_not raise_exception
+    end
+  end
+
+  describe '#expr' do
+    it 'provides expression' do
+      c = Calculator.new '1,2'
+      c.expr.should == '1,2'
+    end
+  end
+
+  describe '#expr=' do
+    it 'allows expression definition' do
+      c = Calculator.new '1,2'
+      c.expr = '3,4'
+      c.expr.should == '3,4'
+    end
   end
 end
