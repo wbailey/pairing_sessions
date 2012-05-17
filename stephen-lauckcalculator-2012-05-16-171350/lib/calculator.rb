373b93333ecaed43f63dd43adb8aeab3d0b8f3b0
Wes Bailey <baywes@gmail.com>
2012-05-16 17:58:10 -0700

    Create a calculator that is initialized with a string expression

--- stephen-lauckcalculator-2012-05-16-171350/lib/calculator.rb
+++ stephen-lauckcalculator-2012-05-16-171350/lib/calculator.rb
@@ -1,2 +1,14 @@
 class Calculator
+
+  def initialize(exp)
+    @expr = exp
+  end
+
+  def expr
+    @expr
+  end
+
+  def expr=(exp)
+    @expr = exp
+  end
 end
--- stephen-lauckcalculator-2012-05-16-171350/spec/calculator_spec.rb
+++ stephen-lauckcalculator-2012-05-16-171350/spec/calculator_spec.rb
@@ -6,7 +6,29 @@ describe Calculator do
     it "should instantiate" do
       expect {
         Calculator.new
+      }.should raise_exception
+    end
+
+    it 'accepts expression' do
+      expect {
+        Calculator.new '1,2'
       }.should_not raise_exception
+    end
+  end
+
+  describe '#expr' do
+    it 'accesses the expression' do
+      c = Calculator.new '1,2'
+      c.expr.should == '1,2'
+    end
+  end
+
+  describe '#expr=' do
+    it 'resets the expression' do
+      c = Calculator.new '1,2'
+      c.expr.should == '1,2'
+      c.expr = '3,4'
+      c.expr.should == '3,4'
     end
   end
 end
