e2ad88e63e55e32109b44f11f1a9d98324e56cb1
Wes Bailey <baywes@gmail.com>
2012-06-22 13:12:10 -0700

    Create a diff method that computes the consecutive differences

--- kevin-rohrbaught-calculator-2012-06-22-120157/lib/calculator.rb
+++ kevin-rohrbaught-calculator-2012-06-22-120157/lib/calculator.rb
@@ -1,12 +1,28 @@
 class Calculator
   attr_accessor :expr
 
-  def initialize(expr = '')
+  def initialize(expr = '1,2')
     @expr = expr
-    @digits = Array(expr.split(',')).map(&:to_i)
+    parse_expr
+  end
+
+  def expr=(expr)
+    @expr = expr
+    parse_expr
   end
 
   def add
     @digits.inject(:+) || 0
+  end
+
+  def diff
+    @digits.inject(:-) || 0
+  end
+
+private
+  def parse_expr
+    @digits = Array(expr.split(',')).map(&:to_i)
+
+    raise if @digits.size < 2
   end
 end
--- kevin-rohrbaught-calculator-2012-06-22-120157/spec/calculator_spec.rb
+++ kevin-rohrbaught-calculator-2012-06-22-120157/spec/calculator_spec.rb
@@ -24,20 +24,20 @@ describe Calculator do
       subject.expr = expr
       subject.expr.should == expr
     end
+
+    context "with less than two digits" do
+      it "throws an excpeption with a single digit" do
+        expect { Calculator.new('1') }.should raise_exception
+      end
+
+      it "throws an exception with no digits" do
+        expect { Calculator.new('') }.should raise_exception
+      end
+    end
   end
   
   describe "#add" do
     subject { Calculator.new expr }
-
-    context "with no values" do
-      let(:expr){ '' }
-      its(:add){ should == 0 }
-    end
-
-    context "with one digit" do
-      let(:expr){ '1' }
-      its(:add){ should == 1 }
-    end
 
     context "with two digits" do
       let(:expr){ '1,2' }
@@ -53,5 +53,34 @@ describe Calculator do
       let(:expr) { '1,2,5,8' }
       its(:add) { should == 16 }
     end
+
+    context "after updating expr" do
+      let(:expr) { '1,2' }
+      before do 
+        subject.expr = '1,2,3'
+      end
+
+      its(:add) { should == 6 }
+    end
   end
+
+  describe "#diff" do
+    subject { Calculator.new expr }
+
+    context "with two digits" do
+      let(:expr){ "1,0" }
+      its(:diff){ should == 1 }
+    end
+
+    context "with three digits" do
+      let(:expr){ "3,2,1" }
+      its(:diff){ should == 0 }
+    end
+
+    context "with more than two digits" do
+      let(:expr){ "5,4,3,2,1" }
+      its(:diff){ should == -5 }
+    end
+  end
+
 end
