e7c45f712d5e3eef4285b6d31efc9b0a4b972051
Wes Bailey <baywes@gmail.com>
2012-09-26 16:05:20 -0700

    There is a 3x3 playing grid

--- caitlin_johnson_ascii_tic_tac_toe-2012-09-26-152843/lib/ascii_tic_tac_toe.rb
+++ caitlin_johnson_ascii_tic_tac_toe-2012-09-26-152843/lib/ascii_tic_tac_toe.rb
@@ -1,2 +1,24 @@
 class AsciiTicTacToe
+
+  def initialize
+    @row = [1,2,3]
+    @column = [1,2,3]
+  end
+ 
+  def rows
+    @row.size
+  end
+
+  def columns
+    @column.size
+  end
+
+  def row
+    @row
+  end
+
+  def column
+    @column
+  end
+
 end
--- caitlin_johnson_ascii_tic_tac_toe-2012-09-26-152843/spec/ascii_tic_tac_toe_spec.rb
+++ caitlin_johnson_ascii_tic_tac_toe-2012-09-26-152843/spec/ascii_tic_tac_toe_spec.rb
@@ -9,4 +9,38 @@ describe AsciiTicTacToe do
       }.should_not raise_exception
     end
   end
+
+  describe 'board' do
+    it 'has 3 rows' do
+      subject.rows.should == 3
+    end
+
+    it 'has 3 columns' do
+      subject.columns.should == 3
+    end
+
+    it 'identifies the first row by 1' do
+      subject.row[0].should == 1
+    end
+
+    it 'identifies the first row by 2' do
+      subject.row[1].should == 2
+    end
+
+    it 'identifies the first row by 3' do
+      subject.row[2].should == 3
+    end
+
+    it 'identifies the first column by 1' do
+      subject.column[0].should == 1
+    end
+
+    it 'identifies the first column by 2' do
+      subject.column[1].should == 2
+    end
+
+    it 'identifies the first column by 3' do
+      subject.column[2].should == 3
+    end
+  end
 end
