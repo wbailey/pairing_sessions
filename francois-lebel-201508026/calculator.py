dba59f1417b1a6c4c7a7b631a081492f05c9361b
Wes Bailey <wes@sproutling.com>
2015-08-26 10:54:46 -0700

    Allow the expression to contain an unknown amount of numbers

--- francois-lebel-201508026/calculator.py
+++ francois-lebel-201508026/calculator.py
@@ -1,12 +1,22 @@
 class Calculator(object):
+    def _expr_to_digits(self, expr):
+        if expr == '':
+            return [0]
+        return map(int, expr.split(','))
+
     def __init__(self, expr):
-        self.expr = expr
+        self.numbers = self._expr_to_digits(expr)
 
     def add(self):
-        if self.expr == '':
+        if not self.numbers:
             return 0
-        numbers = self.expr.split(',')
-        if not numbers:
-            return 0
-        return sum(map(int, numbers))
+        return sum(self.numbers)
+
+    @property
+    def expr(self):
+        return ','.join(map(str, self.numbers))
+
+    @expr.setter
+    def set_expr(self, expr):
+        self.numbers = self._expr_to_digits(expr)
 
--- francois-lebel-201508026/calculator_test.py
+++ francois-lebel-201508026/calculator_test.py
@@ -15,17 +15,34 @@ class TestStringMethods(unittest.TestCase):
         calc.expr = new_value
         self.assertEqual(calc.expr, new_value)
 
-    def test_add_two_numbers(self):
-        calc = Calculator("1,2")
-        self.assertEqual(calc.add(), 3)
+    def test_add_no_numbers(self):
+        calc = Calculator("")
+        self.assertEqual(calc.add(), 0)
 
     def test_add_a_single_number(self):
         calc = Calculator("1")
         self.assertEqual(calc.add(), 1)
 
-    def test_add_no_numbers(self):
-        calc = Calculator("")
-        self.assertEqual(calc.add(), 0)
+    def test_add_two_numbers(self):
+        calc = Calculator("1,2")
+        self.assertEqual(calc.add(), 3)
+
+    def test_add_three_numbers(self):
+        calc = Calculator("1,2,3")
+        self.assertEqual(calc.add(), 6)
+
+    def test_add_five_numbers(self):
+        calc = Calculator("1,2,3,4,5")
+        self.assertEqual(calc.add(), 15)
+
+    def test_invalid_digits(self):
+        with self.assertRaises(ValueError):
+            calc = Calculator("a,b")
+
+    def test_invalid_separator(self):
+        with self.assertRaises(ValueError):
+            calc = Calculator("1;2")
+
 
 if __name__ == '__main__':
     unittest.main()
