17f2aeec230ac382eae69535498227de11d8511b
Wes Bailey <wes@sproutling.com>
2016-08-25 15:09:49 -0700

    Create an add method that sums the string expression

--- carl-ek-calculator-2016-08-23-153216/lib/calculator.py
+++ carl-ek-calculator-2016-08-23-153216/lib/calculator.py
@@ -1,9 +1,18 @@
 class Calculator:
 
-    def __init__(self, value=''):
+    def __init__(self, value='', base = 10):
         self.expression = value
+        self.base = base
 
-    def eval_expression(self):
-        # execute the expression
-        pass
+    def add(self):
+        # add comma separated values 
+        # empty string gives 0
+        if len(self.expression) == 0:
+            return 0
+        nums = self.expression.split(',')
+        sum = 0
+        for n in nums:
+            sum += int(n, self.base)
+                
+        return sum
     
--- carl-ek-calculator-2016-08-23-153216/spec/calculator_spec.py
+++ carl-ek-calculator-2016-08-23-153216/spec/calculator_spec.py
@@ -11,6 +11,15 @@ class InstantiateTestCase(unittest.TestCase):
         finally:
             self.assertEqual(result, 1)
 
+    def test_invalid_string_instantiation(self):
+        try:
+            calc = Calculator("a,")
+            result = 0
+        except:
+            result = 1
+        finally:
+            self.assertEqual(result, 1)
+
     def test_null_instantiation(self):
         try:
             calc = Calculator()
@@ -25,6 +34,45 @@ class InstantiateTestCase(unittest.TestCase):
         result = calc.expression
 
         self.assertEqual(result, "1,2")
+
+class AddTestCase(unittest.TestCase):
+    def test_null_string(self):
+        calc = Calculator()
+        result = calc.add()
+
+        self.assertEqual(result, 0)
+
+    def test_single_digit_string(self):
+        calc = Calculator("1")
+        result = calc.add()
+
+        self.assertEqual(result, 1)
+
+    def test_two_digit_string(self):
+        calc = Calculator("1,2")
+        result = calc.add()
+
+        self.assertEqual(result, 3)
+
+    def test_spaces_string(self):
+        calc = Calculator("      ")
+        result = calc.add()
+
+        self.assertEqual(result, 0)
+
+    def test_invalid_string(self):
+        calc = Calculator()
+        calc.expr = "a,b"
+
+        try:
+            result = calc.add()
+        finally:
+            result = -1
+
+        self.assertEqual(result, -1)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
